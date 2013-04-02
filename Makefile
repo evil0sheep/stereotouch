998f0ae4c226ec4f3d46e3403a1391abf0e02cd2
dave <dave@mango.(none)>
2013-04-02 10:45:38 -0700

    intitial commit

---
+++ Makefile
@@ -0,0 +1,50 @@
+
+CC=g++
+NVCC=nvcc
+NVFLAGS=-O3 --profile -arch=compute_20 -code=sm_20 -Xptxas -dlcm=ca 
+PROJECT_ROOT=${PWD}
+OPENCV_SRC_PATH=${PROJECT_ROOT}/opencv
+OPENCV_LIB_PATH=${OPENCV_SRC_PATH}/release
+OPENCV_INCLUDE_PATH=${OPENCV_SRC_PATH}/include
+OPENCV_PKG_CONFIG_PATH=${OPENCV_SRC_PATH}/lib/pkgconfig
+CFLAGS= `pkg-config --cflags --libs opencv` -g 
+SRC_FILES=src/main.cpp
+LIBS=-DGL_GLEXT_PROTOTYPES -lGL -lGLU -lglut
+
+
+EXECUTABLE=-o stereoview
+
+
+
+all: 
+	PKG_CONFIG_PATH=${OPENCV_PKG_CONFIG_PATH}:${PKG_CONFIG_PATH};\
+	export PKG_CONFIG_PATH;\
+	${CC} ${EXECUTABLE} ${SRC_FILES} ${LIBS} ${CFLAGS} ;
+
+cuda:
+	${NVCC} ${NVFLAGS} -c -o ransac.o src/ransac.cu
+
+
+calibration:
+	PKG_CONFIG_PATH=${OPENCV_PKG_CONFIG_PATH}:${PKG_CONFIG_PATH};\
+	export PKG_CONFIG_PATH;\
+	${CC} -o stereo_calib ${OPENCV_SRC_PATH}/samples/cpp/stereo_calib.cpp ${LIBS} ${CFLAGS} ;\
+	${CC} -o calibration ${OPENCV_SRC_PATH}/samples/cpp/calibration.cpp ${LIBS} ${CFLAGS} ;\
+	${CC} -o imagelist_creator ${OPENCV_SRC_PATH}/samples/cpp/imagelist_creator.cpp ${LIBS} ${CFLAGS} ;\
+	${CC} -o image_splitter src/main.cpp src/utils.cpp ${LIBS} ${CFLAGS} ;\
+
+opencv:
+	cd ${OPENCV_LIB_PATH}; \
+	pwd; \
+	cmake -D WITH_CUDA=OFF -D WITH_JPEG=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_PYTHON_SUPPORT=ON .. > ${PROJECT_ROOT}/cmake_output.txt ;\
+	#make; \
+	sudo make install; \
+
+opencv_GPU:
+	cd ${OPENCV_LIB_PATH}; \
+	pwd; \
+	cmake -D CUDA_ARCH_BIN = "2.1" -D CUDA_ARCH_PTX = "empty" -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_PYTHON_SUPPORT=ON .. > ${PROJECT_ROOT}/cmake_output.txt ;\
+	make; \
+	sudo make install; \
+
+
---
+++ src/main.cpp
@@ -0,0 +1,88 @@
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#define MAX_FRAME_DT (1000.0 / 60)
+
+ 
+using namespace cv;
+
+
+
+int main()
+{
+    Mat frameL, frameR, frameLUnRotated, frameRUnRotated;
+    double timestampL,timestampR;
+    VideoCapture capL(1); 
+    VideoCapture capR(2); 
+    if(!capL.isOpened() || !capR.isOpened() ){
+        fprintf(stderr, "Failed to open stereo capture device\n");
+        return -1;
+    }  // check if we succeeded
+        
+
+    capL >> frameL;
+    capR >> frameR;
+
+
+    //Window to show livefeed
+    cvNamedWindow("LiveFeedL",CV_WINDOW_AUTOSIZE);
+    cvNamedWindow("LiveFeedR",CV_WINDOW_AUTOSIZE);
+
+    Mat rotMatL = cvCreateMat(2,3,CV_32FC1);
+    Mat rotMatR = cvCreateMat(2,3,CV_32FC1);
+    // Compute rotation matrix
+    CvPoint2D32f centerL = cvPoint2D32f( frameL.cols/2, frameL.rows/2 );
+    rotMatL = getRotationMatrix2D( centerL, 180, 1 );
+
+    CvPoint2D32f centerR = cvPoint2D32f( frameR.cols/2, frameR.rows/2 );
+    rotMatR = getRotationMatrix2D( centerR, 0, 1 );
+
+
+    frameL = Mat();
+    frameR = Mat();
+    while(1)
+    {
+        //printf("test value = %f\n", capL.get(propID));
+       
+        //Load the next frame
+        capL.grab();
+        capR.grab();
+        timestampL = capL.get(CV_CAP_PROP_POS_MSEC);
+        timestampR = capR.get(CV_CAP_PROP_POS_MSEC);
+
+        while (abs(timestampL - timestampR) > MAX_FRAME_DT){
+            if (timestampR > timestampL) {
+                capL.grab();
+                timestampL = capL.get(CV_CAP_PROP_POS_MSEC);
+            } else {
+                capR.grab();
+                timestampR = capR.get(CV_CAP_PROP_POS_MSEC);
+            }
+        }
+        printf("timestamp difference = %f\n", timestampL - timestampR);
+
+        if(!(
+            capL.retrieve(frameLUnRotated) &&
+            capR.retrieve(frameRUnRotated))){
+
+            fprintf(stderr, "failed to capture stereo frame\n");
+            break;
+        }
+        frameL= Mat();
+        warpAffine( frameLUnRotated, frameL, rotMatL, frameL.size() );
+        warpAffine( frameRUnRotated, frameR, rotMatR, frameR.size() );
+        
+
+
+        //Show the present frame
+        imshow("LiveFeedL",frameL);
+        imshow("LiveFeedR",frameR);
+        //Escape Sequence
+        char c=cvWaitKey(33);
+        //If the key pressed by user is Esc(ASCII is 27) then break out of the loop
+        if(c=='q')
+           break;
+    }
+    //CleanUp
+
+    cvDestroyAllWindows();
+}
