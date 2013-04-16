e5af4b3dab7f49e89cae4e5697ab82b309c1f71a
dave <dave@mango.(none)>
2013-04-16 10:16:50 -0700

    updated for new camera

---
+++ src/record.cpp
@@ -0,0 +1,97 @@
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#define MAX_FRAME_DT (1000.0 / 60)
+
+ 
+using namespace cv;
+
+
+
+int main()
+{
+   printf("begin\n");
+    Mat frameL, frameR;
+    double timestampL,timestampR;
+
+    VideoCapture capL(2); 
+    VideoCapture capR(1); 
+    if(!capL.isOpened() || !capR.isOpened() ){
+        fprintf(stderr, "Failed to open stereo capture device\n");
+        return -1;
+    }  // check if we succeeded
+        
+
+    capL >> frameL;
+    capR >> frameR;
+
+    printf("size\n");
+    Size combinedSize(frameL.cols + frameR.cols, frameR.rows);
+    printf("writer\n");
+    VideoWriter output("output.mpeg", CV_FOURCC('P','I','M','1'), 30.0, combinedSize);
+    printf("mat\n");
+    Mat frameCombined(combinedSize, frameL.type());
+
+    
+   
+    cvNamedWindow("LiveFeed",CV_WINDOW_AUTOSIZE);
+
+
+    frameL = Mat();
+    frameR = Mat();
+    while(1)
+    {
+        //printf("test value = %f\n", capL.get(propID));
+       
+        //Load the next frame
+        capL.grab();
+        capR.grab();
+        timestampL = capL.get(CV_CAP_PROP_POS_MSEC);
+        timestampR = capR.get(CV_CAP_PROP_POS_MSEC);
+
+        while (abs(timestampL - timestampR) > MAX_FRAME_DT){
+            if (timestampR > timestampL) {
+                capL.grab();
+                timestampL = capL.get(CV_CAP_PROP_POS_MSEC);
+            } else {
+                capR.grab();
+                timestampR = capR.get(CV_CAP_PROP_POS_MSEC);
+            }
+            //printf("timestamp difference = %f\n", timestampL - timestampR);
+        }
+       // 
+
+        if(!(
+            capL.retrieve(frameL) &&
+            capR.retrieve(frameR))){
+
+            fprintf(stderr, "failed to capture stereo frame\n");
+            break;
+        }
+
+        printf("left\n");
+        frameL.copyTo(frameCombined(Rect(0,0,frameL.cols, frameL.rows)));
+        printf("right\n");
+        frameR.copyTo(frameCombined(Rect(frameL.cols, 0,  frameR.cols, frameR.rows)));
+        printf("done\n");
+
+        imshow("LiveFeed",frameCombined);
+        
+
+        
+        char c = cvWaitKey(10);
+        //If the key pressed by user is Esc(ASCII is 27) then break out of the loop
+
+        if(c == 'q')
+           break;
+
+        if(c == 'x'){
+            VideoCapture temp = capL;
+            capL = capR;
+            capR = temp;
+        }
+           
+    }
+    //CleanUp
+
+    cvDestroyAllWindows();
+}
